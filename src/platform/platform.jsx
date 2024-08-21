dc731c1acf158868f7aa2ededb1f1749e2f1e598
git config --global user.name <abddallahmohammed21@gmail.com>
2024-08-22 02:12:21 +0300

    clickable template 0.1

--- src/platform/platform.jsx
+++ src/platform/platform.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Website1 from '../websites/website1/website1'
 import './platform.css'
+import website1image from "../assets/websitesimages/website1.jfif"
 export default function Platform() {
   return (
     <div className='platformmainpage'>
         <div className='websiteborder'>
-            <Website1 />
+            <div className='websiteimagecontainer'>
+   <img className='websiteimage' src={website1image}></img>
+   </div>
         </div>
     </div>
   )
