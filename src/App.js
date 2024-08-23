d8c4700514993c51f4c1ff0198add79ec594ab95
git config --global user.name <abddallahmohammed21@gmail.com>
2024-08-23 09:57:36 +0300

    added 'view'&'edit' buttons

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import logo from './logo.svg';
 import Website1 from './websites/website1/website1';
 import React ,{useState} from 'react';
 import Platform from './platform/platform';
+import { BrowserRouter as Router , Route , Routes } from 'react-router-dom';
+
 function App() {
   const [editable,seteditable] = useState(true)
   return (
-    <div className='container'>
-<Platform />
-</div>
+     <Router>
+    <Routes>
+      <Route path="/" element={<Platform />} />
+      <Route path="/website1" element={<Website1 />} />
+    </Routes>
+  </Router>
   );
 }
 
--- src/platform/platform.jsx
+++ src/platform/platform.jsx
@@ -2,12 +2,22 @@ import React from 'react'
 import Website1 from '../websites/website1/website1'
 import './platform.css'
 import website1image from "../assets/websitesimages/website1.jfif"
+import { Link } from 'react-router-dom'
 export default function Platform() {
   return (
     <div className='platformmainpage'>
         <div className='websiteborder'>
+    
             <div className='websiteimagecontainer'>
-   <img className='websiteimage' src={website1image}></img>
+
+              <Link to="/website1">
+   <img className='websiteimage' src={website1image} ></img>
+   </Link>
+   
+   </div>
+   <div className='buttons'>
+   <button className='viewbutton websitebutton'><Link to='/website1'>View</Link></button>
+   <button className='editbutton websitebutton'>Edit</button>
    </div>
         </div>
     </div>
