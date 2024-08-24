5ae1ac3322101e469cbefc4451ee95cfe14fe814
git config --global user.name <abddallahmohammed21@gmail.com>
2024-08-24 03:41:15 +0300

    back button added

--- src/websites/website1/website1.jsx
+++ src/websites/website1/website1.jsx
@@ -6,10 +6,19 @@ import { MdOutlineArtTrack } from "react-icons/md";
 import { MdDeveloperBoard } from "react-icons/md";
 import { LiaHandshake } from "react-icons/lia";
 import './website1.css';
-
+import { FaBackward } from "react-icons/fa";
+import { Link } from 'react-router-dom';
 export default function Website1() {
   return (
+    
 <div className='website1container'>
+ <div className='goback'>
+  <div className='iconntext'>
+    <Link to='/' className='iconntext'>
+  <FaBackward className='backicon'/> <p> Back to Main page </p>
+  </Link>
+  </div>
+ </div>
     <div className='website1'>
     <div className='homepage'>
       <header>
