46c1aee3b69d2435304e3959164eb17fd710ab89
git config --global user.name <abddallahmohammed21@gmail.com>
2024-08-27 23:06:55 +0300

    website2 added for testing

--- src/platform/platform.jsx
+++ src/platform/platform.jsx
@@ -11,12 +11,12 @@ export default function Platform({editable , seteditable}) {
             <div className='websiteimagecontainer'>
 
               <Link to="/landingpage1">
-   <img className='websiteimage' src={website1image} ></img>
+   <img className='websiteimage' src={website1image}></img>
    </Link>
    
    </div>
    <div className='buttons'>
-   <Link to='/landingpage1'> 
+   <Link to='/landingpage1'>
    <button className='viewbutton websitebutton'>View</button>
    </Link>
    <Link to='/landingpage1' onClick={() => seteditable(true)}>
