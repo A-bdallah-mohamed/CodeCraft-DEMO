fa71ee89419a8c43bfaebc3f709d5bf1f1739be1
git config --global user.name <abddallahmohammed21@gmail.com>
2024-09-02 23:24:02 +0300

    updated form design

--- src/components/formtext.jsx
+++ src/components/formtext.jsx
@@ -4,7 +4,7 @@ import React, {useState,useEffect} from 'react'
 import './forms.css'
 
 export default function formtext({newtext,position,formvisible,setformvisible}) {
-
+//changed design
   return (
 
         <form
@@ -12,9 +12,12 @@ export default function formtext({newtext,position,formvisible,setformvisible}) 
             className='formtext'
             style={{ left: position.x, top: position.y, position: 'absolute' }}
         >
-            <div onClick={()=>setformvisible(false)}>X</div>
-            <input onChange={newtext.handlechange} placeholder={newtext.text}/>
+            <div onClick={()=>setformvisible(false)} className='closelogo'>X</div>
+            <label for="text">Text</label>
+            <div className='inputnbutton'>
+            <input onChange={newtext.handlechange} placeholder={newtext.text} id='text'/>
             <button type='submit'>Submit</button>
+            </div>
         </form>
     )
     
