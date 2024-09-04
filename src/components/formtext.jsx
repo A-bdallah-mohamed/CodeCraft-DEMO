f0d295b11da1a83d2c2b5a9fa31d63f264c63111
git config --global user.name <abddallahmohammed21@gmail.com>
2024-09-04 06:11:27 +0300

    changed form design and added undo feature

--- src/components/formtext.jsx
+++ src/components/formtext.jsx
@@ -4,19 +4,19 @@ import React, {useState,useEffect} from 'react'
 import './forms.css'
 
 export default function formtext({newtext,position,formvisible,setformvisible}) {
-//changed design
   return (
 
         <form
             onSubmit={newtext.handlesubmit}
-            className='formtext'
+            className='formtext formappeared'
             style={{ left: position.x, top: position.y, position: 'absolute' }}
         >
             <div onClick={()=>setformvisible(false)} className='closelogo'>X</div>
-            <label for="text">Text</label>
+            <label for="text" >Text</label>
             <div className='inputnbutton'>
             <input onChange={newtext.handlechange} placeholder={newtext.text} id='text'/>
             <button type='submit'>Submit</button>
+            <button onClick={newtext.undo}>undo</button>
             </div>
         </form>
     )
--- src/custom hooks/Changetext.jsx
+++ src/custom hooks/Changetext.jsx
@@ -3,21 +3,31 @@ import Formtext from "../components/formtext";
 const useChangetext = (text1,condition,setvisible) =>{
     const [text , settext ] = useState(text1)
     const [newtext,setnewtext] = useState("")
+    const [prevtext,setprevtext] = useState("")
     const handlechange = (e)  => {
         setnewtext(e.target.value)
     }
     const handlesubmit = (e) => {
 e.preventDefault()
 if(condition){
+    setprevtext(text)
     settext(newtext)
     setvisible(false)
 }}
+const undo =()=>{
+    setnewtext(prevtext)
+    setprevtext(text)
+    settext(newtext)
+    console.log("current text is : ",text,"prevtext is : ",prevtext)
+}
     return { 
         text, 
         newtext, 
         handlechange, 
         handlesubmit, 
-        setnewtext
+        setnewtext,
+        prevtext,
+        undo
       };
 }
 export default useChangetext;
